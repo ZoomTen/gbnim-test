8a3612a48b14e37975f311377de46cba0c1db958
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-09-12 23:44:29 +0700

    me when

--- src/panicoverride.nim
+++ src/panicoverride.nim
@@ -4,8 +4,14 @@
 ## These two functions need to be defined/overridden as well,
 ## you can put in anything here or even nothing at all
 
-proc panic(s: string) =
-  discard
-
 proc rawoutput(s: string) =
-  discard
+  # TODO: replace this with a proper
+  # crash screen
+  var pt: uint16 = 0xc000
+  for i in 0 ..< len(s):
+    cast[ptr char](pt)[] = s[i]
+    inc pt
+
+proc panic(s: string) =
+  rawoutput(s)
+  asm """stop"""
